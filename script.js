ced9651f16b95ce8bdd6fb7c5c9fffdc91991de5
Jacob <120258067+jacob70421@users.noreply.github.com>
2024-09-04 19:54:36 +0200

    fourth commit
    open source for unity projects, edited text, font

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 class Project {
-    constructor(name, color, border, date, link, thumbnail) {
+    constructor(name, color, border, date, openSource, link, thumbnail) {
         this.name = name;
         this.color = color;
         this.border = border;
         this.date = date;
+        this.openSource = openSource;
         this.link = link;
         this.thumbnail = thumbnail;
     }
 }
 
-let ProjectList = [
-    new Project("Cargo Security", "red", "2px solid red", "May 12, 2024", "https://diabolicalcats.itch.io/cargo-security", "https://img.itch.zone/aW1nLzE2OTE2OTk1LnBuZw==/315x250%23c/Ex7%2BRY.png"),
-    new Project("Crack The Code And Get A Cat", "blue", "2px solid blue", "August 26, 2024", "https://diabolicalcats.itch.io/crack-the-code-and-get-a-cat", "https://img.itch.zone/aW1nLzE3NTQ0OTU2LnBuZw==/315x250%23c/qul3cI.png")
+let UnityProjectList = [
+    new Project("Cargo Security", "red", "2px solid red", "May 12, 2024", "No", "https://diabolicalcats.itch.io/cargo-security", "https://img.itch.zone/aW1nLzE2OTE2OTk1LnBuZw==/315x250%23c/Ex7%2BRY.png"),
+    new Project("Crack The Code And Get A Cat", "blue", "2px solid blue", "August 26, 2024", "Yes", "https://diabolicalcats.itch.io/crack-the-code-and-get-a-cat", "https://img.itch.zone/aW1nLzE3NTQ0OTU2LnBuZw==/315x250%23c/qul3cI.png")
 ];
-GenerateProjectList();
+GenerateUnityProjectList();
 
-function GenerateProjectList() {
-    let parent = document.querySelector(".container");
-    for (let i = 0; i < ProjectList.length; i++) {
+function GenerateUnityProjectList() {
+    let parent = document.querySelector(".unityprojectscontainer");
+    for (let i = 0; i < UnityProjectList.length; i++) {
         let child = document.createElement("a");
         let thumbnail = document.createElement("img");
         let textName = document.createElement("b");
         let textDate = document.createElement("p");
-        child.href = ProjectList[i].link;
-        child.style.cssText = "border:" + ProjectList[i].border + "; border-radius: 10px; margin: 1px; display: inline-grid";
-        thumbnail.src = ProjectList[i].thumbnail;
+        let textOpenSource = document.createElement("p");
+        child.href = UnityProjectList[i].link;
+        child.style.cssText = "border:" + UnityProjectList[i].border + "; border-radius: 10px; margin: 1px; display: inline-grid";
+        thumbnail.src = UnityProjectList[i].thumbnail;
         thumbnail.style.cssText = "margin-left: auto; margin-right: auto";
-        textName.style.cssText = "color:" + ProjectList[i].color + "; margin: 7px; padding-top: 7px";
-        textDate.style.cssText = "color:" + ProjectList[i].color + "; margin: 7px; padding-bottom: 7px";
-        textName.innerHTML = ProjectList[i].name;
-        textDate.innerHTML = "Published: " + ProjectList[i].date;
+        textName.style.cssText = "color:" + UnityProjectList[i].color + "; margin: 7px; padding-top: 7px";
+        textDate.style.cssText = "color:" + UnityProjectList[i].color + "; margin-top: 7px;";
+        textOpenSource.style.cssText = "color:" + UnityProjectList[i].color + "; margin-bottom: 20px";
+        textName.innerHTML = UnityProjectList[i].name;
+        textDate.innerHTML = "Published: " + UnityProjectList[i].date;
+        textOpenSource.innerHTML = "Is Open Source: " + UnityProjectList[i].openSource;
         child.appendChild(thumbnail);
         child.appendChild(textName);
         child.appendChild(textDate);
+        child.appendChild(textOpenSource);
         parent.appendChild(child);
     }
 }
