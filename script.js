956672e0a259631e9ce31d959de2a9644ddc56cc
Jacob <120258067+jacob70421@users.noreply.github.com>
2024-09-06 21:38:46 +0200

    fifth commit
    blogs, cleaned, navigation bar

--- script.js
+++ script.js
@@ -9,15 +9,26 @@ class Project {
         this.thumbnail = thumbnail;
     }
 }
+class Blog {
+    constructor(name, description, blogFile) {
+        this.name = name;
+        this.description = description;
+        this.blogFile = blogFile;
+    }
+}
 
 let UnityProjectList = [
     new Project("Cargo Security", "red", "2px solid red", "May 12, 2024", "No", "https://diabolicalcats.itch.io/cargo-security", "https://img.itch.zone/aW1nLzE2OTE2OTk1LnBuZw==/315x250%23c/Ex7%2BRY.png"),
     new Project("Crack The Code And Get A Cat", "blue", "2px solid blue", "August 26, 2024", "Yes", "https://diabolicalcats.itch.io/crack-the-code-and-get-a-cat", "https://img.itch.zone/aW1nLzE3NTQ0OTU2LnBuZw==/315x250%23c/qul3cI.png")
 ];
+let BlogList = [
+    new Blog("Test Blog", "This is a test blog.", "/Blogs/blog-0.html")
+];
 GenerateUnityProjectList();
+GenerateBlogList();
 
 function GenerateUnityProjectList() {
-    let parent = document.querySelector(".unityprojectscontainer");
+    let parent = document.querySelector("#unityprojects");
     for (let i = 0; i < UnityProjectList.length; i++) {
         let child = document.createElement("a");
         let thumbnail = document.createElement("img");
@@ -27,7 +38,6 @@ function GenerateUnityProjectList() {
         child.href = UnityProjectList[i].link;
         child.style.cssText = "border:" + UnityProjectList[i].border + "; border-radius: 10px; margin: 1px; display: inline-grid";
         thumbnail.src = UnityProjectList[i].thumbnail;
-        thumbnail.style.cssText = "margin-left: auto; margin-right: auto";
         textName.style.cssText = "color:" + UnityProjectList[i].color + "; margin: 7px; padding-top: 7px";
         textDate.style.cssText = "color:" + UnityProjectList[i].color + "; margin-top: 7px;";
         textOpenSource.style.cssText = "color:" + UnityProjectList[i].color + "; margin-bottom: 20px";
@@ -38,6 +48,21 @@ function GenerateUnityProjectList() {
         child.appendChild(textName);
         child.appendChild(textDate);
         child.appendChild(textOpenSource);
+        parent.appendChild(child);
+    }
+}
+function GenerateBlogList() {
+    let parent = document.querySelector("#blogs");
+    for (let i = 0; i < BlogList.length; i++) {
+        let child = document.createElement("a");
+        let title = document.createElement("p");
+        let description = document.createElement("p");
+        child.href = BlogList[i].blogFile;
+        title.innerHTML = BlogList[i].name;
+        title.style = "font-weight: bold;"
+        description.innerHTML = BlogList[i].description;
+        child.appendChild(title);
+        child.appendChild(description);
         parent.appendChild(child);
     }
 }
